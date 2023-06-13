229dd4b49005dd92a5d5948dc9bb24c61a4ca2f2
Michaella Moreno <michaella.moreno@gmail.com>
2023-06-13 15:21:28 -0500

    answered number 1-7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+//  #1
         int myFavoriteNumber = 3;
+//  #2
         String myString = "Michaella is amazing!";
-
-
-
-
-
-
+//  #3 Change your code to assign a character value to myString. What do you notice?
+//        char myString = "Michaella is amazing!";
+//        java: not a statement
+//  #4 Change your code to assign the value 3.14159 to myString. What happens?
+//        String myString = 3.14159;
+//        java: variable myString is already defined in method main(java.lang.String[])
+//  #5  Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//        long myNumber = ;
+//        System.out.println(myNumber);
+//        java: illegal start of expression
+//  #6 Change your code to assign the value 3.14 to myNumber. What do you notice?
+//        long myNumber = (long) 3.14;
+//   get screamed at by intellij. intellij instructed to re-write long myNumber = 3.14; to long myNumber = (long) 3.14;
+//  #7  Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+    long myNumber = 123L;
 
 
     }
