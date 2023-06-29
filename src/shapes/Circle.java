049ee9cfe3769c5c35aed515c105093d3d820480
Michaella Moreno <michaella.moreno@gmail.com>
2023-06-29 16:46:53 -0400

    added and completed shapes package.

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,26 @@
 package shapes;
 
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+    public double getCircumference(){
+        return 2 * Math.PI * this.radius;
+    }
+
+    public static void main(String[] args) {
+        Circle mycircle = new Circle(5);
+        System.out.println("mycircle.getArea() = " + mycircle.getArea());
+        System.out.println("mycircle.getCircumference() = " + mycircle.getCircumference());
+    }
+
+
+
 }
