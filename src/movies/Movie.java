bb02bebfc99aec88ed2f9929e7aff8491b9675b2
Michaella Moreno <michaella.moreno@gmail.com>
2023-07-21 12:36:10 -0400

    Completed movie application exercise.

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,33 @@
 package movies;
-
+// and a constructor that sets both of these. Create methods to access these properties and change them (getters and setters).
 public class Movie {
+//   private fields for name and category
+    private String name;
+    private String category;
+//The constructor that sets both of the name and category
+// The constructor will always be the same as the class
+// The constructor has 2 parameters: name and category
+// this keyword is used to refer to the current object of the movie class.
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+
+    }
+
+// Create Getters and Setters:
+     public String getName(){
+        return name;
+     }
+    public String setName(String name ){
+        this.name = name;
+        return name;
+    }
+     public String getCategory(){
+        return category;
+     }
+    public String setCategory(String category){
+        this.category = category;
+        return category;
+    }
+
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,4 +1,88 @@
 package movies;
 
+import util.Input;
+
+
 public class MoviesApplication {
+
+//    Method to print all movies.
+    private static void viewAllMovies(){
+
+        Movie[] movies = MoviesArray.findAll();
+        System.out.println("All Movies: ");
+
+        for (Movie movie : movies){
+            System.out.println(movie.getName() + "--" + movie.getCategory());
+        }
+        System.out.println();
+    }
+
+//    Method for specific Movies:
+    private static void chooseMoviesByCategory(String category){
+        Movie[] movies = MoviesArray.findAll();
+        System.out.println(" Moivie Category: " + category);
+        for (Movie movie : movies ){
+            if (movie.getCategory().equalsIgnoreCase(category)){
+                System.out.println(movie.getName());
+            }
+        }
+        System.out.println();
+    }
+//    exit app
+    public static void exitList (int status) {
+        Runtime.getRuntime().exit(status);
+    }
+// Method to run the Movie Application
+
+public static void runMovieApp() {
+    Input input = new Input();
+    do {
+        System.out.println("What would you like to do?\n");
+        System.out.println("0 - exit\n" +
+                "1 - View All Movies\n" +
+                "2 - View Movies in the Animated Category\n" +
+                "3 - View Movies in the Drama Category\n" +
+                "4 - View Movies in the Horror Category\n" +
+                "5 - View Movies in the Sci-Fi Category\n");
+        int userschoice = input.getInt();
+        System.out.println();
+            if (userschoice == 1){
+                viewAllMovies();
+            } else if(userschoice == 2){
+                chooseMoviesByCategory("animated");
+            } else if (userschoice == 3) {
+                chooseMoviesByCategory("drama");
+            } else if (userschoice == 4) {
+                chooseMoviesByCategory("horror");
+            } else if (userschoice == 5) {
+                chooseMoviesByCategory("scifi");
+            } else if (userschoice == 0) {
+                System.out.println("Exiting Movie application.....");
+                exitList(0);
+            }
+
+    } while (true);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        runMovieApp();
+    }
+
+
+
+
 }
